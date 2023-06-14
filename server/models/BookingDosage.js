3c00ae90308b15e9e494f0f31ee09f637e1f47df
Barath Kumar S <92674796+BARATHB-K@users.noreply.github.com>
2023-06-14 07:59:55 +0530

    Update BookingDosage.js

--- server/models/BookingDosage.js
+++ server/models/BookingDosage.js
@@ -3,14 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookingSchema = new Schema({
-    idv: {
-        type: String,
-        required: true,
-    },
-    centre: {
-        type: String,
-        required: true,
-    },
     date: {
         type: Date,
         required: true,
